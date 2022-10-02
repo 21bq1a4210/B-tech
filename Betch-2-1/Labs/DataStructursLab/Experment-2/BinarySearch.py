6ee686ad376a449ae1dca79e4cf87bd649573d48
21bq1a4210 <21bq1a4210@vvit.net>
2022-10-02 14:52:11 +0530

    DS BinarySearch uploded

--- Betch-2-1/Labs/DataStructursLab/Experment-2/BinarySearch.py
+++ Betch-2-1/Labs/DataStructursLab/Experment-2/BinarySearch.py
@@ -1,15 +1,22 @@
-def BS(arr,key):
-    low,high=0,len(arr)-1
-    while low<=high:
-        mid=(low+high)//2
-        if key==arr[mid]:
-            return f"element found at {mid+1}"
-        elif key>mid:
-            low=mid+1
+class BinarySearch:
+    def __init__(self,arr,key):
+        self.arr=arr
+        self.key=key
+        BinarySearch.search(self)
+    @staticmethod
+    def search(self):
+        low,high=0,len(arr)-1
+        while low<=high:
+            mid=(low+high)//2
+            if key==arr[mid]:
+                print(f"element found at {mid+1}")
+                break
+            elif key>mid:
+                low=mid+1
+            else:
+                high=mid-1
         else:
-            high=mid-1
-    else:
-        return "element not found"
+            print("element not found")
 arr=list(map(int,(input("enter elements:").split())))
 key=int(input("enter key:"))
-print(BS(arr,key))
+obj=BinarySearch(arr,key)

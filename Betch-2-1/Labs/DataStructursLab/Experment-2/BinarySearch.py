111fe02427278adf9cd539c8e324b3a8a703c047
21bq1a4210 <21bq1a4210@vvit.net>
2022-10-02 14:38:43 +0530

    BinarySearch in Functions added

--- Betch-2-1/Labs/DataStructursLab/Experment-2/BinarySearch.py
+++ Betch-2-1/Labs/DataStructursLab/Experment-2/BinarySearch.py
@@ -1,15 +1,15 @@
-arr=[1,2,3,4,5]
-key=10
-low,high=0,len(arr)-1
-while low<=high:
-    mid=(low+high)//2
-    if key==arr[mid]:
-        print(mid+1)
-        break
-    elif key>mid:
-        low=mid+1
+def BS(arr,key):
+    low,high=0,len(arr)-1
+    while low<=high:
+        mid=(low+high)//2
+        if key==arr[mid]:
+            return f"element found at {mid+1}"
+        elif key>mid:
+            low=mid+1
+        else:
+            high=mid-1
     else:
-        high=mid-1
-else:
-    print("element not found")
-    
+        return "element not found"
+arr=list(map(int,(input("enter elements:").split())))
+key=int(input("enter key:"))
+print(BS(arr,key))

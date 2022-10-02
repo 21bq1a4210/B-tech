d926f7b3b44f73ed0faa7a82b2752730000fdb41
21bq1a4210 <21bq1a4210@vvit.net>
2022-10-02 21:26:45 +0530

    BubbleSort in calss

--- Betch-2-1/ClassWork/DataStructurs/Unit-1/Sorting/BubbleSort/BubbleSort.py
+++ Betch-2-1/ClassWork/DataStructurs/Unit-1/Sorting/BubbleSort/BubbleSort.py
@@ -1,8 +1,16 @@
+class BubbleSort:
+    def __init__(self,a):
+        self.a=a
+        BubbleSort._Sort(self)
+    @staticmethod  
+    def _Sort(self):
+        for i in range(len(a)):
+            for j in range(len(a)-i-1):
+                if a[j]>a[j+1]:
+                    a[j],a[j+1]=a[j+1],a[j]
+                    print(f"iteration:{i+1} res:{a}")
+        print()
+        print(f"final sorted result:{a}")
 a=[20,3,17,19,25,35,9,42,16,27]
-for i in range(len(a)):
-    for j in range(len(a)-i-1):
-        if a[j]>a[j+1]:
-            a[j],a[j+1]=a[j+1],a[j]
-            print(f"iteration:{i+1} res:{a}")
-print()
-print(f"final sorted result:{a}")
+obj=BubbleSort(a)
+

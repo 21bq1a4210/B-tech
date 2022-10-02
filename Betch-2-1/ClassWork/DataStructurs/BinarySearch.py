b7f9262c2ff03140bde0e25ff54f9401b18e1803
21bq1a4210 <21bq1a4210@vvit.net>
2022-10-02 21:20:11 +0530

    BubbleSort in functions uploded

---
+++ Betch-2-1/ClassWork/DataStructurs/BinarySearch.py
@@ -0,0 +1,11 @@
+def BubbleSort(a):
+    for i in range(len(a)):
+        for j in range(len(a)-i-1):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
+                print(f"iteration:{i+1} res:{a}")
+    print()
+    print(f"final sorted result:{a}")
+a=[20,3,17,19,25,35,9,42,16,27]
+BubbleSort(a)
+

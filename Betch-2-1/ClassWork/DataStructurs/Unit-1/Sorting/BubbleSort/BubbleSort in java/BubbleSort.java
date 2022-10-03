aacc67f135ecb8d2d0184c45ee38493bdaaaf945
21bq1a4210 <21bq1a4210@vvit.net>
2022-10-03 09:04:20 +0530

    BubbleSort in py and java uploded

--- Betch-2-1/ClassWork/DataStructurs/Unit-1/Sorting/BubbleSort/BubbleSort in java/BubbleSort.java
+++ Betch-2-1/ClassWork/DataStructurs/Unit-1/Sorting/BubbleSort/BubbleSort in java/BubbleSort.java
@@ -1,16 +1,17 @@
 class Demo{
     public static void main(String[] args) {
-        int []a={3, 20, 17, 19, 25, 35, 9, 42, 16, 27};
+        int []a={9,23,21,11,43,33};
         for(int i=0;i<a.length;i++){
             for(int j=0;j<a.length-i-1;j++){
                 if(a[j]>a[j+1]){
                     int temp=a[j];
                     a[j]=a[j+1];
                     a[j+1]=temp;
+                    System.out.print("iteration:"+(i+1)+" ");
                 }
             }
         }
-        System.out.print("sorted array:");
+
         for(int i=0;i<a.length;i++){
             System.out.print(a[i]+" ");
         }

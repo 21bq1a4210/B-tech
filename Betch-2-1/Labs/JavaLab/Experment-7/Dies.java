93691ab9cdb34c42ba8d05ee72840e6627815265
21bq1a4210 <21bq1a4210@vvit.net>
2022-10-09 21:04:41 +0530

    Exp-7 updated

--- Betch-2-1/Labs/JavaLab/Experment-7/Dies.java
+++ Betch-2-1/Labs/JavaLab/Experment-7/Dies.java
@@ -1,25 +1,33 @@
-import java.util.Random; 
+import java.util.Random; //importing Random class
+class PairDies{//start of PairDies class
+    int arr[]=new int[6];//creating and alocating memory for arr which used to store the result
+    PairDies(){//start of constructor
+        random();//invoking the random method of PairDies class
+        display();//invoking the display method of PairDies class
+    }//end of constructor
 
-class generateRandom{ 
-
-	public static void main(String args[]) 
-	{ 
-        int arr[]=new int[6];
-		Random rand = new Random(); 
-        for(int i=0;i<1000;i++){
-            int rand_int1 = rand.nextInt(1,7); 
-		    int rand_int2 = rand.nextInt(1,7);
-            if(rand_int1==rand_int2){
+    private void random(){//start of private random method (rolling a pair of dies)
+		Random rand = new Random(); //creating an obj of Random class and allocating memory 
+        for(int i=0;i<10_000;i++){//start of for loop iterating over 10k time by the pb
+            int rand_int1 = rand.nextInt(1,7);//storing a random in in b/w 1 to 6 in rand_int1
+		    int rand_int2 = rand.nextInt(1,7);//storing a random in in b/w 1 to 6 in rand_int2
+            if(rand_int1==rand_int2){//if both are equal then increasing the arr[index-1]
                 int j=rand_int1;
                 j--;
-                arr[j]+=1;
+                arr[--rand_int1]+=1;
             }
-		    /*System.out.println("Random Integers: "+rand_int1); 
-		    System.out.println("Random Integers: "+rand_int2);
-            */
-        } 
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]+" ");
         }
+    }
+   
+    private void display(){
+        for(int i=0;i<arr.length;i++){
+            System.out.println("("+(i+1)+","+(i+1)+")"+"="+arr[i]);
+        }
+    }
+        
+}
+class Main{ 
+    public static void main(String args[]) { 
+        PairDies pd=new PairDies();
 	} 
 } 

3ec9b800cb40713a7e3782db9a36ac311ff8cc63
21bq1a4210 <21bq1a4210@vvit.net>
2022-10-11 06:32:13 +0530

    conversion description added

--- Betch-2-1/ClassWork/Java/Unit-1/3.Data Types, Variables,and Arrays/9.Conversion.java
+++ Betch-2-1/ClassWork/Java/Unit-1/3.Data Types, Variables,and Arrays/9.Conversion.java
@@ -14,4 +14,10 @@ class Conversion {
     b = (byte) d;
     System.out.println("d and b " + d + " " + b);   // d and b 323.142 67
     }
+    /*
+     * When the value 257 is cast into a byte variable, the result is the
+    remainder of the division of 257 by 256 (the range of a byte), which is 1 in this case. When
+    the d is converted to an int, its fractional component is lost. When d is converted to a byte, its
+    fractional component is lost, and the value is reduced modulo 256, which in this case is 67.
+     */
 };

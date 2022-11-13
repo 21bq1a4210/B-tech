43fb5c7727b56e8e121907c0f0b79afbbf671a4a
21bq1a4210 <21bq1a4210@vvit.net>
2022-11-13 12:22:46 +0530

    updated mathopp

--- Betch-2-1/New/Go/6.MathOpp.go
+++ Betch-2-1/New/Go/6.MathOpp.go
@@ -8,4 +8,10 @@ func main() {
 	//fmt.Println(2 + int(2.2))
 	/*.\6.mathopp.go:8:22:
 	cannot convert 2.2 (untyped float constant) to type int*/
+	fmt.Println(5.5 - 5)   //.5
+	fmt.Println(3.5 / 5)   //.7
+	fmt.Println(3.5 / 0.5) //7
+	fmt.Println(-10 / 2)   //-5
+	fmt.Println(10 / -2)   //-5
+	fmt.Println(1 / 2)     //0
 }

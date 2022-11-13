af86daf96508ed4ec8a62634b296a785abd83eb8
21bq1a4210 <21bq1a4210@vvit.net>
2022-11-13 12:29:08 +0530

    updated

--- Betch-2-1/New/Go/6.MathOpp.go
+++ Betch-2-1/New/Go/6.MathOpp.go
@@ -3,15 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(1 + 2)
+	fmt.Println(1 + 2) //3
+
 	fmt.Println(1 + 2.2) //no error
 	//fmt.Println(2 + int(2.2))
-	/*.\6.mathopp.go:8:22:
-	cannot convert 2.2 (untyped float constant) to type int*/
+	/*
+		.\6.mathopp.go:8:22:
+		cannot convert 2.2 (untyped float constant) to type int
+	*/
+
 	fmt.Println(5.5 - 5)   //.5
 	fmt.Println(3.5 / 5)   //.7
 	fmt.Println(3.5 / 0.5) //7
 	fmt.Println(-10 / 2)   //-5
 	fmt.Println(10 / -2)   //-5
 	fmt.Println(1 / 2)     //0
+	fmt.Println(4 <= 4.0)  //true
+	fmt.Println('A' == 65) //true
+
+	//fmt.Println('A' == "A")
+	/*
+		.\6.mathopp.go:19:21: invalid operation:
+		'A' == "A" (mismatched types untyped rune and untyped string)
+	*/
 }

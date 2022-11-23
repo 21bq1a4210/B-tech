8bbe9dc18558a7e8dcdeeba5fc78781fb5f71ae5
21bq1a4210 <21bq1a4210@vvit.net>
2022-11-24 00:38:21 +0530

    cooments are added

--- B-tech-2-1/Labs/JavaLab/Experment-11/DemoException.java
+++ B-tech-2-1/Labs/JavaLab/Experment-11/DemoException.java
@@ -1,15 +1,16 @@
 import java.util.InputMismatchException;
+import java.lang.ArithmeticException;
 import java.util.Scanner;
 class DemoException{
     public static void main(String[] args) {
         try{
             Scanner input=new Scanner(System.in);
             System.out.print("Enter numerator and denominator:");
-            int numerator=input.nextInt();
-            int denominator=input.nextInt();
+            int numerator=input.nextInt();  //chance of throwing InputMismatchException
+            int denominator=input.nextInt();    //chance of throwing InputMismatchException
             input.close();
-            float c=numerator/denominator;
-            System.out.println("Result:"+c);
+            float c=numerator/denominator;  //chane of throwing ArithmeticException
+            System.out.println("Result:"+c);  
         }catch(InputMismatchException e){
             System.out.println("Invalid input:");
             e.printStackTrace();

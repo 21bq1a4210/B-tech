a47cad0bbda742701634b2589792aa0ea2f2c8cf
21bq1a4210 <21bq1a4210@vvit.net>
2022-12-04 20:48:35 +0530

    exp-16 uploded

---
+++ B-tech-2-1/Labs/DataStructursLab/Experment-16/binHeapOpp.py
@@ -0,0 +1,53 @@
+def max_heapify(A,k):
+    l = left(k)
+    r = right(k)
+    if l < len(A) and A[l] > A[k]:
+        largest = l
+    else:
+        largest = k
+    if r < len(A) and A[r] > A[largest]:
+        largest = r
+    if largest != k:
+        A[k], A[largest] = A[largest], A[k]
+        max_heapify(A, largest)
+
+def left(k):
+    return 2 * k + 1
+
+def right(k):
+    return 2 * k + 2
+
+def build_max_heap(A):
+    n = int((len(A)//2)-1)
+    for k in range(n, -1, -1):
+        max_heapify(A,k)
+
+def min_heapify(A,k):
+    l = left(k)
+    r = right(k)
+    if l < len(A) and A[l] < A[k]:
+        smallest = l
+    else:
+        smallest = k
+    if r < len(A) and A[r] < A[smallest]:
+        smallest = r
+    if smallest != k:
+        A[k], A[smallest] = A[smallest], A[k]
+        min_heapify(A, smallest)
+
+def left(k):
+    return 2 * k + 1
+
+def right(k):
+    return 2 * k + 2
+
+def build_min_heap(A):
+    n = int((len(A)//2)-1)
+    for k in range(n, -1, -1):
+        min_heapify(A,k)
+
+A = [3,9,2,1,4,5]
+build_min_heap(A)
+print("min heap:",A)
+build_max_heap(A)
+print("max heap:",A)

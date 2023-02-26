c65a6955e794a555eaae2298fcdf5ac4fb977b85
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-02-26 19:19:51 +0530

    Update VectorIntro.R

--- B-tech-2-2/ClassWork/R prg/VectorIntro.R
+++ B-tech-2-2/ClassWork/R prg/VectorIntro.R
@@ -16,3 +16,46 @@ u<-t[c(2,5,7)]
 print(u)
 #output: [1] "mon" "thr" "sat"
 
+#ACCESSING VECTOR USING LOGICAL INDEXING:
+u<-t[c(FALSE,TRUE,FALSE,TRUE,TRUE,FALSE,TRUE)]
+print(u)
+#output:  [1] "mon" "wed" "thr" "sat"
+
+#ACCESSING VECTOR USING NEGATIVE INDEXING:
+u<-t[c(-2,-5,-7)]
+print(u)
+#output:  [1] "sun" "tue" "wed" "fri"
+
+#VECTOR OPERATIONS:
+  #VECTOR ARITHEMEATIC:
+v1<-c(3,5,6,2,1,7)
+v2<-c(4,7,8,1,2,3)
+
+print(v1+v2)   #ADD
+#output:  [1]  7 12 14  3  3 10
+
+print(v1-v2)  #SUB
+#output:  [1] -1 -2 -2  1 -1  4
+
+print(v1*v2)  #MUL
+#output:  [1] 12 35 48  2  2 21
+
+print(v1/v2)  #DIV
+#output:  [1] 0.7500000 0.7142857 0.7500000 2.0000000 0.5000000 2.3333333
+
+# print(v1%v2)  #MOD
+# Error: unexpected input in "print(v1%v2)"
+
+#VECTOR ELEMENTS SORTING:
+print(sort(v1)) 
+#output:  [1] 1 2 3 5 6 7
+
+print(sort(v1,decreasing = TRUE)) #revers order
+#output: [1] 7 6 5 3 2 1
+
+#VECTOR CHARACTER SORTING:
+print(sort(t))
+#output:  [1] "fri" "mon" "sat" "sun" "thr" "tue" "wed"
+
+print(sort(t,decreasing = TRUE)) #revers order
+#output:  [1] "wed" "tue" "thr" "sun" "sat" "mon" "fri"

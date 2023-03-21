e4213bed5d3df51fb585d65e10636671c36c23c8
21bq1a4210 <21bq1a4210@vvit.net>
2023-03-21 18:47:20 +0530

    started in jetBrains

--- Projects/kivy-thelab/main.py
+++ Projects/kivy-thelab/main.py
@@ -1,11 +1,37 @@
+import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+'''
+class GridLayoutExample(GridLayout):
+    pass
+'''
+
+class BoxLayoutExample(BoxLayout):
+    '''def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        b3 = Button(text="C")
+
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3)
+    '''
 
 class MainWidget(Widget):
     pass
+
 
 class TheLabApp(App):
     pass
 
 TheLabApp().run()
- 

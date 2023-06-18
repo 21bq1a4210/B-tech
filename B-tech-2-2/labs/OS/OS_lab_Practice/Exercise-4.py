670d5c962cb010b0d42e8c9a714689f8f6485ed3
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-06-18 21:00:21 +0530

    Ex-4.4
    MFU

--- B-tech-2-2/labs/OS/OS_lab_Practice/Exercise-4.py
+++ B-tech-2-2/labs/OS/OS_lab_Practice/Exercise-4.py
@@ -1,6 +1,6 @@
-pl=[1,3,0,3,5,6,3]
+pl = [ 6, 1, 1, 2, 0, 3, 4, 6, 0, 2, 1, 2, 1, 2, 0, 3, 2, 1, 2, 0]
 cap=3
-fr=[]
+
 #FIFO
 def FIFO(pl,cap):
     fr=[]
@@ -16,7 +16,7 @@ def FIFO(pl,cap):
         else:
             ph+=1
     return pf,ph
-#print(FIFO(pl,cap))
+print(FIFO(pl,cap))
     
 #LRU
 def LRU(pl,cap):
@@ -36,6 +36,48 @@ def LRU(pl,cap):
         else:
             ph+=1
     return pf,ph
-#print(LRU(pl,cap))
+print(LRU(pl,cap))
+
 #LFU
+def LFU(pl,cap):
+    fr={}
+    lru=[]
+    ph,pf=0,0
+    for pg in pl:
+        if pg not in fr:
+            if len(fr)<cap:
+                fr[pg]=1
+                #print(fr)
+            else:
+                rm=min(fr,key=lambda k: fr[k])
+                del fr[rm]
+                fr[pg]=1
+                #print(fr)
+            pf+=1
+        else:
+            fr[pg]+=1
+            ph+=1
+    return pf,ph
+print(LFU(pl,cap))
+                
 #MFU
+def MFU(pl,cap):
+    fr={}
+    lru=[]
+    ph,pf=0,0
+    for pg in pl:
+        if pg not in fr:
+            if len(fr)<cap:
+                fr[pg]=1
+                #print(fr)
+            else:
+                rm=max(fr,key=lambda k: fr[k])
+                del fr[rm]
+                fr[pg]=1
+                #print(fr)
+            pf+=1
+        else:
+            fr[pg]+=1
+            ph+=1
+    return pf,ph
+print(MFU(pl,cap))

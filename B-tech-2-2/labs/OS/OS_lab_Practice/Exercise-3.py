b00acae802f64b14c16c0c7656cafadaba22ef2a
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-06-18 19:57:11 +0530

    ex-3.2
    prevenction

--- B-tech-2-2/labs/OS/OS_lab_Practice/Exercise-3.py
+++ B-tech-2-2/labs/OS/OS_lab_Practice/Exercise-3.py
@@ -1,5 +1,3 @@
-##deadlock_avoid
-
 n,m=5,3
 alloc=[[0,1,0],[2,0,0],[3,0,2],[2,1,1],[0,0,2]]
 max=[[7,5,3],[3,2,2],[9,0,2],[2,2,2],[4,3,3]]
@@ -11,11 +9,27 @@ need=[[max[i][j]-alloc[i][j] for j in range(m)]
 fin=[0]*n
 seq=[]
 
+# ##deadlock_avoid
+# while 0 in fin:
+#     for i in range(n):
+#         if fin[i]==0 and all([need[i][j] <= avil[j] for j in range(m)]):
+#             fin[i]=1
+#             seq.append(i)
+#             avil=[alloc[i][j]+avil[j] for j in range(m)]
+# print(seq)
+
 while 0 in fin:
+    dl=1
     for i in range(n):
         if fin[i]==0 and all([need[i][j] <= avil[j] for j in range(m)]):
             fin[i]=1
             seq.append(i)
             avil=[alloc[i][j]+avil[j] for j in range(m)]
-print(seq)
+            dl=0
+if dl:
+    print("dead lock occured")
+else:
+    print("dead lock not occured")
+    print(seq)
+    
             

3f3f252d0fb376dbe69ab7f3452ac24aa6959e47
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-06-18 20:38:24 +0530

    Ex-4.2
    LRU

--- B-tech-2-2/labs/OS/OS_lab_Practice/Exercise-4.py
+++ B-tech-2-2/labs/OS/OS_lab_Practice/Exercise-4.py
@@ -16,8 +16,26 @@ def FIFO(pl,cap):
         else:
             ph+=1
     return pf,ph
-print(FIFO(pl,cap))
+#print(FIFO(pl,cap))
     
 #LRU
+def LRU(pl,cap):
+    fr=[]
+    lru=[]
+    ph,pf=0,0
+    for pg in pl:
+        if pg not in fr:
+            if len(fr)<cap:
+                fr.append(pg)
+            else:
+                lru=[fr.index(i) for i in fr]
+                lru.sort()
+                fr.pop(lru[0])
+                fr.append(pg)
+            pf+=1
+        else:
+            ph+=1
+    return pf,ph
+#print(LRU(pl,cap))
 #LFU
 #MFU

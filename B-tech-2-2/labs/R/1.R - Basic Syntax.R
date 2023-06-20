6170ef4a5885cea5a16734f94bed6867ae3b7356
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-06-20 19:29:43 +0530

    Started R prg

---
+++ B-tech-2-2/labs/R/1.R - Basic Syntax.R
@@ -0,0 +1,17 @@
+myString <- 'Hello'
+print(myString)
+
+'Save the above code in a file test.R and execute it at Linux command prompt as given below. Even if you are using Windows or other system, syntax will remain same.
+$ Rscript test.R' 
+
+if(FALSE) {
+  "This is a demo for multi-line comments and it should be put inside either a 
+      single OR double quote"
+}
+
+myString <- "Hello, World!"
+print ( myString)
+
+'Though above comments will be executed by R interpreter, 
+they will not interfere with your actual program. You should put such comments inside, 
+either single or double quote.'

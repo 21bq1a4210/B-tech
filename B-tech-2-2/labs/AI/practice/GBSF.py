7ab6bc82a11395863a31a4de25a67aef1bd1aaa3
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-06-20 19:27:35 +0530

    Update GBSF.py

--- B-tech-2-2/labs/AI/practice/GBSF.py
+++ B-tech-2-2/labs/AI/practice/GBSF.py
@@ -1,31 +1,41 @@
-def best_first_search(graph, start):
+from queue import PriorityQueue
+
+def best_first_search(graph, start, goal):
+    # Create a priority queue to store the nodes to be explored
+    queue = PriorityQueue()
+    # Enqueue the start node with priority 0
+    queue.put((0, start))
+    # Create a set to keep track of visited nodes
     visited = set()
-    priority_queue = [start]
     
-    while priority_queue:
-        node = priority_queue.pop(0)
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            neighbors = graph[node]
-            neighbors.sort()  # Sort the neighbors alphabetically
-            priority_queue = neighbors + priority_queue
+    while not queue.empty():
+        # Dequeue the node with the highest priority
+        current_cost, current_node = queue.get()
+        
+        # Check if the current node is the goal node
+        if current_node == goal:
+            print("Goal found!")
+            return
+        
+        # Mark the current node as visited
+        visited.add(current_node)
+        
+        # Explore the neighbors of the current node
+        for neighbor, cost in graph[current_node]:
+            # Enqueue the unvisited neighbors with their heuristic values as priority
+            if neighbor not in visited:
+                queue.put((cost, neighbor))
     
-# Graph representation
+    print("Goal not found!")
+
+# Example usage:
 graph = {
-    'A': ['D', 'B'],
-    'B': ['C', 'E', 'G'],
-    'C': ['A'],
-    'D': ['C'],
-    'E': ['H'],
-    'F': [],
-    'G': ['F'],
-    'H': ['G', 'F']
+    'A': [('B', 5), ('C', 2)],
+    'B': [('D', 4), ('E', 3)],
+    'C': [('F', 1)],
+    'D': [],
+    'E': [('F', 3)],
+    'F': []
 }
 
-# Start node
-start_node = 'A'
-
-# Perform Best First Search
-print("Order of nodes visited using Best First Search:")
-best_first_search(graph, start_node)
+best_first_search(graph, 'A', 'F')

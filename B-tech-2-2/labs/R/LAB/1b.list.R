74a593da5835a55035ad871a50d28ea5681e86f7
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-06-22 18:00:50 +0530

    Create List
    1b.list with strings, vectors and logical values

---
+++ B-tech-2-2/labs/R/LAB/1b.list.R
@@ -0,0 +1,26 @@
+l<-list(
+  str_valu<-'hello R prg',
+  num_vector<-c(1:10),
+  logic_vector<-c(TRUE,FALSE,TRUE),
+  char_vector<-c('apple','bpple','cpple'),
+  logical_value<-FALSE
+)
+
+print(l)
+'
+output:
+[[1]]
+[1] "hello R prg"
+
+[[2]]
+ [1]  1  2  3  4  5  6  7  8  9 10
+
+[[3]]
+[1]  TRUE FALSE  TRUE
+
+[[4]]
+[1] "apple" "bpple" "cpple"
+
+[[5]]
+[1] FALSE
+'

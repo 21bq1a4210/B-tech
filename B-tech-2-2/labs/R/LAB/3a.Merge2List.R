05b074da15a2470df4ded6fec9de07e14ebeeaeb
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-06-22 18:41:14 +0530

    created 3a.Merge@list
    Implement R Script to merge two or more lists

---
+++ B-tech-2-2/labs/R/LAB/3a.Merge2List.R
@@ -0,0 +1,15 @@
+l1<-list(
+  name='a',
+  age=30
+)
+
+l2<-list(
+  city='yo nagar',
+  occupation='student'
+)
+
+print(merge(l1,l2))
+'output:
+  name age     city occupation
+1    a  30 yo nagar    student
+'

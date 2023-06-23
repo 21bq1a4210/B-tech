930859b0b2369411288ab2d1da6d5cf3dbf37bfe
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-06-23 05:36:18 +0530

    Create 8a.MeanModeMedian.R
    Implement R Script to perform mean, median, mode, range, summary,
    variance, standarddeviation operations

---
+++ B-tech-2-2/labs/R/LAB/8a.MeanModeMedian.R
@@ -0,0 +1,26 @@
+data<-c(1,2,3,4,5,6,7,8,9)
+
+#mean
+print(mean(data)) #output: [1] 5
+
+#median:
+print(median(data)) #output: 1] 5
+
+#mode:
+print(mode(data))
+
+#range:
+print(range(data)) #output: 1 9
+
+#summary:
+print(summary(data))
+'output:
+ Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+      1       3       5       5       7       9 
+'
+
+#varance:
+print(var(data)) #output:[1] 7.5
+
+#satandard devation:
+print(sd(data)) #output: [1] 2.738613

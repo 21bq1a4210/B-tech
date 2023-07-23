9386d7a3b6323d16c781330f421ab803dc0f527c
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-07-23 21:16:13 +0530

    defining a one to many relationship

--- AdvnceLearning/Dj/storefont/store/models.py
+++ AdvnceLearning/Dj/storefont/store/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
-# Create your models here.
-class Product(models.Model):
-    # Dj model field types: https://docs.djangoproject.com/en/4.2/ref/models/fields/
 
-    title = models.CharField(max_length=255) #varchar(255)
+# Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2) #ex: 9999.99
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_pudate = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
+    collections = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 class Customer(models.Model):
-    MEMBERSHIP_BRONZE = 'B' #makes easy to change in the feature
-    MEMBERSHIP_SILVER = 'S' #makes easy to change in the feature
-    MEMBERSHIP_GOLD = 'G' #makes easy to change in the feature
+    MEMBERSHIP_BRONZE = 'B'  # makes easy to change in the feature
+    MEMBERSHIP_SILVER = 'S'  # makes easy to change in the feature
+    MEMBERSHIP_GOLD = 'G'  # makes easy to change in the feature
 
     MEMBERSHIP_CHOICE = [
         (MEMBERSHIP_BRONZE, 'Bronze'),
@@ -26,6 +30,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICE, default=MEMBERSHIP_BRONZE)
+
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
@@ -40,9 +45,27 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICE, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
-    
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()

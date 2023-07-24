12da3c13f251b1d80b4038548a40c162e4af9489
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-07-24 22:29:11 +0530

    Generic relationship

--- AdvnceLearning/Dj/storefont/like/models.py
+++ AdvnceLearning/Dj/storefont/like/models.py
@@ -1,15 +1,11 @@
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 
 # Create your models here.
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # Type (product, video, article)
-    # ID
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()

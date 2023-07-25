24a100acf8e0a5a8d92c9292f674ed36f7894c76
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-07-25 19:19:37 +0530

    Generating dummy data
    https://mockaroo.com/

--- AdvnceLearning/Dj/storefont/store/models.py
+++ AdvnceLearning/Dj/storefont/store/models.py
@@ -73,3 +73,7 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+'''
+To generate dummy data: https://mockaroo.com/
+'''

a375b96a129a69abc49e22d88246d8a89d16233c
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-07-26 21:00:46 +0530

    managers and query sets

--- AdvnceLearning/Dj/storefont/playground/views.py
+++ AdvnceLearning/Dj/storefont/playground/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 # Create your views here.
 # req --> resp
 # req handler
 # action
 
-
-
 def say_hello(request):
     # Pull data from db
     # Transform data
     #    send emails
-
+    query_set = Product.objects.all()
+    # for product in query_set:
+    #     print(product)
+    # list(query_set)
+    #  query_set[0]
+    # query_set.filter().filter().order_by()
+    
     #return HttpResponse('Hello World')
     return render(request, 'home.html', {'name': 'Sarath'})

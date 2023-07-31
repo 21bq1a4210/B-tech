45d180a78be25e23963df98e09b217b5c04f16cb
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-07-31 10:57:24 +0530

    Update main.py
    modified time()
    added wishMe()

--- Projects/VoiceAssistant/main.py
+++ Projects/VoiceAssistant/main.py
@@ -3,18 +3,24 @@ import datetime
 
 engine =pyttsx3.init() #Ava
 engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-engine.setProperty('rate', 135)
+engine.setProperty('rate', 140)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
 def time():
-    tim = datetime.datetime.now().strftime("%I:%M:%S")
-    speak(tim)
+    tim = datetime.datetime.now().strftime("%I:%M")
+    return f"The current time is: {tim}"
 
 def date():
     year = int(datetime.datetime.now().year)
     month = int(datetime.datetime.now().month)
     date = int(datetime.datetime.now().day)
-    speak(f"{date} {month} {year}")
-data()
+    return (f"The current date is: {date} {month} {year}")
+
+def wishMe():
+    speak('Welcome... back')
+    speak(time())
+    speak(date())
+    speak('Hello there, I am Ava. how can I be of service')
+wishMe()

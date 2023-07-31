774f52f26dea2b0d7f6f3b9da018ecf1c0a974c3
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-07-31 11:14:09 +0530

    Update main.py
    modified rate
    added more functionality to the wishMe()

--- Projects/VoiceAssistant/main.py
+++ Projects/VoiceAssistant/main.py
@@ -3,7 +3,7 @@ import datetime
 
 engine =pyttsx3.init() #Ava
 engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-engine.setProperty('rate', 140)
+engine.setProperty('rate', 143)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
@@ -19,7 +19,17 @@ def date():
     return (f"The current date is: {date} {month} {year}")
 
 def wishMe():
-    speak('Welcome... back')
+    def wishTime():
+        hr = int(datetime.datetime.now().strftime('%I'))
+        if 5 <= hr >= 11:
+            return 'Morning'
+        elif 12 <= hr >= 17:
+            return 'afternoon'
+        elif 18 <= hr >= 22:
+            return 'evening'
+        else:
+            return 'night'
+    speak(f'Good {wishTime()}. Welcome back')
     speak(time())
     speak(date())
     speak('Hello there, I am Ava. how can I be of service')

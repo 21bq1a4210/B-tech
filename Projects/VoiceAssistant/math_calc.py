71f2d9f65077d7f3861e97a940fc638d1419f2a7
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-08-01 06:01:19 +0530

    updated math_calc.py
    added more mathematical operations

--- Projects/VoiceAssistant/math_calc.py
+++ Projects/VoiceAssistant/math_calc.py
@@ -10,16 +10,25 @@ math_operations = {
     'divide by': '/',
     'square': '**2',
     'cube': '**3',
-    'power': '**'
+    'power': '**',
+    'sin': 'math.sin',
+    'cos': 'math.cos',
+    'tan': 'math.tan',
+    'pi': 'math.pi',
+    'e': 'math.e',
+    'factorial': 'math.factorial'
 }
 
 def calculate(expression):
     expression = expression.replace('calculate', '')
+    split = expression.split()
+    split = [(num.replace(num, f'({num})') if num.isdigit() else num) for num in split]
+    expression = ''.join(x for x in split)
     try:
         for word, opp in math_operations.items():
             expression = expression.replace(word, opp)
 
         result = eval(expression)
-        return result
+        return round(result, 2)
     except Exception as e:
         return str(e)

68bbd38104450947552522a7e08d44a686690cda
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-08-09 21:52:25 +0530

    created a superuser
    python manage.py createsuperuser

--- AdvnceLearning/Dj/storefont/storefront/settings.py
+++ AdvnceLearning/Dj/storefont/storefront/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sessions',
     'playground',
     'debug_toolbar',
     'store',

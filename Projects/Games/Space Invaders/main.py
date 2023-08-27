ef78f92ebbf81aafe93870f4edad354ac2a863eb
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-08-27 10:11:24 +0530

    Score added

--- Projects/Games/Space Invaders/main.py
+++ Projects/Games/Space Invaders/main.py
@@ -22,6 +22,11 @@ pygame.display.set_caption("Space Invader")
 icon = pygame.image.load("Images/ufo.png")
 pygame.display.set_icon(icon)
 
+#Score
+score_value = 0
+font = pygame.font.Font("Font/jd_led3.ttf", 32)
+textX, textY = 10, 10
+
 # player
 playerImg = pygame.image.load("Images/player.png")
 playerX = 370
@@ -31,20 +36,25 @@ playerX_change = 0
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
-# game loop
-running = True
-while running:
-    # RGB
-    screen.blit(background, (0, 0))
+def show_score(x, y):
+    score = font.render(f"Score: {str(score_value)}", True, (255, 255, 255))
+    screen.blit(score, (x, y))
+if __name__ == "__main__":
+    # game loop
+    running = True
+    while running:
+        # RGB
+        screen.blit(background, (0, 0))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
 
-    player(playerX, playerY)
-    pygame.display.update()
+        player(playerX, playerY)
+        show_score(textX, textY)
+        pygame.display.update()
 
-# quit pygame
-pygame.quit()
+    # quit pygame
+    pygame.quit()

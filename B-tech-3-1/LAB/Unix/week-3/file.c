7a73b9c68b484a38631fff3954d76e50c9560848
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2023-10-10 14:00:55 +0530

    updated

---
+++ B-tech-3-1/LAB/Unix/week-3/file.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+//this is main
+int main()
+{
+    cout<<"Hello World";
+    //inside the main
+    return 0;
+}
+//outside the main
---
+++ B-tech-3-1/LAB/Unix/week-3/output.c
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    /* This is a C++ single-line comment
+
+    /*
+    This is a C-style multi-line comment.
+    It spans multiple lines.
+    */
+
+    int x = 10; /* Another C++ single-line comment
+
+    /*
+    This is another C-style multi-line comment.
+    It also spans multiple lines.
+    */
+
+    /* Another C++ single-line comment
+
+    return 0;
+}
+

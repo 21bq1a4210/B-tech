afffdd00cd82b6d7cf6341084067bbd5bd5be4c5
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2024-03-06 19:47:19 +0530

    FS lab updated

---
+++ B-tech-3-2/Lab/FS/nodejs/sarath.js/counter/src/App.js
@@ -0,0 +1,32 @@
+import React , { useState } from "react";
+import Button from "./Button";
+import "./App.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () =>{
+    setCount(count + 1);
+  };
+  let decrementCount = () =>{
+    setCount(count - 1);
+  };
+  return (
+    <div className="app">
+      <div>
+      <div className="title-container">
+        <h2>COUNTER REACT APP</h2>
+        <div className= " title-underline"> </div>
+      </div>
+      <div class="count">
+        <h3>Count:</h3>
+        <h1>{count}</h1>
+      </div>
+      <div class="buttons">
+        <Button title= {"-"} action={decrementCount} />
+        <Button title= {"+"} action={incrementCount} />
+      </div>
+    </div>
+    </div>
+    );
+  }
---
+++ B-tech-3-2/Lab/FS/nodejs/sarath.js/counter/src/Button.js
@@ -0,0 +1,5 @@
+import React from "react";
+export default function Button(props) {
+    let { action, title    }= props;
+    return <button onClick={action}>{title}</button>;
+}

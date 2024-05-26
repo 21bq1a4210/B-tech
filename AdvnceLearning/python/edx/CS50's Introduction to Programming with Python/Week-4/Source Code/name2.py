5dbf0a87ec00284c04a47adcd6e00af3f01d1bf7
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2024-05-26 17:21:13 +0530

    source code

---
+++ AdvnceLearning/python/edx/CS50's Introduction to Programming with Python/Week-4/Source Code/name2.py
@@ -0,0 +1,10 @@
+# Adds error checking
+
+import sys
+
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("hello, my name is", sys.argv[1])
---
+++ AdvnceLearning/python/edx/CS50's Introduction to Programming with Python/Week-4/Source Code/name3.py
@@ -0,0 +1,10 @@
+# Demonstrates sys.exit
+
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+
+print("hello, my name is", sys.argv[1])
